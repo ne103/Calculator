2046859a588fc0fa572e228ddb2ae819538bc2a3
ne103 <ne97@naver.com>
2024-04-29 19:14:29 +0900

    Leve1-1

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -6,9 +6,30 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = sc.nextInt();// Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int firstNumber = 0;
+        while (true) {
+            System.out.print("첫 번째 양의 정수를 입력하세요: ");
+            int i = sc.nextInt();
+
+            if (i < 0) {
+                System.out.println("양의 정수만 입력하세요.");
+            } else {
+                firstNumber = i;
+                break;
+            }
+        }
+
+        int secondNumber = 0;
+        while (true) {
+            System.out.print("두 번째 양의 정수를 입력하세요: ");
+            int i = sc.nextInt();
+
+            if (i < 0) {
+                System.out.println("양의 정수만 입력하세요.");
+            } else {
+                secondNumber = i;
+                break;
+            }
+        }
     }
 }
